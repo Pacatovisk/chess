d16b1a6fb7deb9ddeae2132422748982e469d076
Pacatovisck <marcelovianadiaspacato@gmail.com.br>
2022-01-08 22:38:22 -0300

    First class: Position

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,7 +1,11 @@
 package application;
 
+import boardgame.Position;
+
 public class Program {
     public static void main(String[] args) {
-        System.out.println("Olá mundo!");
+
+        Position position = new Position(1, 2);
+        System.out.println(position);
     }
 }
